074ec666e82c3e4f9676da776e6d2a6cb94a32c9
lamerjack <enry@inwind.it>
2021-01-11 16:50:27 +0100

    added retroarch 1.7.6

---
+++ retroarch176.sh
@@ -0,0 +1,2 @@
+tar -czvf data/retroarch176.tar.gz /home/pi/RetroPie-Setup/scriptmodules/emulators/retroarch /home/pi/RetroPie-Setup/scriptmodules/emulators/retroarch.sh
+
--- updatefix.sh
+++ updatefix.sh
@@ -1,5 +1,5 @@
 sudo tar -zxvf repofix.tar.gz -C /
 sudo apt-get update
-sudo apt-get install g++-4.9 zlib1g apt apt-utils
+sudo apt-get install g++-4.9 zlib1g apt apt-utils tar linux-libc-dev libbz2-1.0 bzip2
 sudo dpkg -i rapidjson-dev.deb
 cp "Update RetrOrangePi.sh" "/home/pi/RetrOrangePi/Update RetrOrangePi.sh"

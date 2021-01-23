b0ae28f6547d5dec9969ea91aa9cc1f8409c4431
lamerjack <enry@inwind.it>
2021-01-23 18:56:51 +0100

    Update updatefix.sh

--- updatefix.sh
+++ updatefix.sh
@@ -5,10 +5,9 @@ if [ "$(id -u)" != "0" ]; then
 echo "run as root!"
 exit 1
 fi
-sudo tar -zxvf repofix.tar.gz -C /
 sudo apt-get update
 sudo apt-get -y install apt apt-utils apt-transport-https libapt-inst1.5 libarchive13 tar bzip2 libbz2-1.0 zlib1g dnsutils unzip wget patch
 sudo apt-get -y install g++-4.9 linux-libc-dev libsdl-image1.2 libpython2.7 libpython2.7-dev
 sudo apt-get -y install dbus dbus-x11 bluetooth libbluetooth-dev libbluetooth3 bluez dosfstools e2fsprogs isc-dhcp-client isc-dhcp-common samba openssh-client ntfs-3g
-sudo dpkg -i data/rapidjson-dev.deb
+#sudo dpkg -i data/rapidjson-dev.deb
 

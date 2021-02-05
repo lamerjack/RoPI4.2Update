fad6afbc567f7f8f2d9390654a399b6c573637ad
lamerjack <enry@inwind.it>
2021-02-05 17:13:16 +0100

    Update Update RetroPie-Setup.sh

--- Update RetroPie-Setup.sh
+++ Update RetroPie-Setup.sh
@@ -1,3 +1,3 @@
 cp scripts/abuse.sh /home/pi/RetroPie-Setup/scriptmodules/ports/
-#sudo tar -zxvf data/RetroPie-Setup.tar.gz -C /
+sudo tar -zxvf data/Amiberry.tar.gz -C /
 sudo tar -zxvf data/flappybird.tar.gz -C /
